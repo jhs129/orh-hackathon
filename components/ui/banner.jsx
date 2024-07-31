157574778d7927305d9da526117dcc5f3f9f7768
jhs129 <john.h.schneider@gmail.com>
2024-07-31 12:50:13 -0400

    fixed formatting

--- components/ui/banner.jsx
+++ components/ui/banner.jsx
@@ -15,22 +15,22 @@ function Banner(props) {
 
   return (
     <section>
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2 bg-primaryLight">
-        <img
-          loading="lazy"
-          src={image}
-          alt={altText}
-          className="md:flex md:absolute md:object-cover md:inset-0 md:w-1/2 h-80"
-        />
+      <div className="flex flex-col md:flex-row md:h-80">
+        <div className="md:w-1/2 bg-primaryLight flex-shrink-0">
+          <img
+            loading="lazy"
+            src={image}
+            alt={altText}
+            className="w-full h-full object-cover"
+          />
         </div>
-        <div className="md:w-1/2 px-8 py-8  bg-secondaryDark h-80">
-            <h1 className="text-primaryAccent">{headline}</h1>
-            <h2 className="text-primaryDark">{subheadline}</h2>
-            <p className="mt-4">{blurb}</p>
-            <Link href={buttonUrl || "#"} passHref>
-              <button>{buttonText}</button>
-            </Link>
+        <div className="md:w-1/2 px-8 py-8 bg-secondaryDark flex flex-col justify-center">
+          <h1 className="text-primaryAccent">{headline}</h1>
+          <h2 className="text-primaryDark">{subheadline}</h2>
+          <p className="mt-4">{blurb}</p>
+          <Link href={buttonUrl || "#"} passHref>
+            <button>{buttonText}</button>
+          </Link>
         </div>
       </div>
     </section>
